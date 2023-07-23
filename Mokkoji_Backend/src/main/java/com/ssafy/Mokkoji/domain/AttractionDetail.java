215a1d75fe87f840488a02a96d786876f46c90cb
JJiWoNing <jjiwoning12@gmail.com>
2023-07-23 20:03:02 +0900

    fix: Entity 클래스 기본 생성자 추가

--- Mokkoji_Backend/src/main/java/com/ssafy/Mokkoji/domain/AttractionDetail.java
+++ Mokkoji_Backend/src/main/java/com/ssafy/Mokkoji/domain/AttractionDetail.java
@@ -1,11 +1,15 @@
 package com.ssafy.Mokkoji.domain;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class AttractionDetail {
 
 	@Id
@@ -28,5 +32,5 @@ public class AttractionDetail {
 		this.modifiedTime = modifiedTime;
 		this.bookTour = bookTour;
 	}
-	
+
 }
