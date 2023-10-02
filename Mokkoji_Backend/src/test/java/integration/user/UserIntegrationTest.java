bd06206cec81b8ebc5a6f9031b86bc463d2ddb22
JJiWoNing <jjiwoning12@gmail.com>
2023-10-03 00:31:46 +0900

    test: 로그인 인수 테스트 추가

--- Mokkoji_Backend/src/test/java/integration/user/UserIntegrationTest.java
+++ Mokkoji_Backend/src/test/java/integration/user/UserIntegrationTest.java
@@ -1,6 +1,8 @@
 package integration.user;
 
 import com.ssafy.Mokkoji.core.user.dto.request.UserJoinRequest;
+import com.ssafy.Mokkoji.core.user.dto.request.UserLoginRequest;
+import com.ssafy.Mokkoji.core.user.dto.response.TokenResponse;
 import integration.IntegrationTest;
 import integration.helper.RestAssuredUtils;
 import io.restassured.response.ExtractableResponse;
@@ -30,5 +32,32 @@ class UserIntegrationTest extends IntegrationTest {
 
         // then
         assertThat(result.statusCode()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    @Test
+    @DisplayName("아이디 비밀번호를 입력하면 로그인에 성공한다.")
+    void login() {
+        // given
+        UserJoinRequest signUpRequest = new UserJoinRequest(
+                "hello123",
+                "hello123",
+                "hello123@mail.com",
+                "최탐탐",
+                "tamtam1@",
+                "01012341234");
+
+        RestAssuredUtils.post("/user/signup", signUpRequest);
+
+        UserLoginRequest userLoginRequest = new UserLoginRequest("hello123", "tamtam1@");
+
+        // when
+        ExtractableResponse<Response> result = RestAssuredUtils.post("/user/login", userLoginRequest);
+
+        // then
+        TokenResponse response = result.as(TokenResponse.class);
+        assertThat(result.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.getAccessToken()).isNotBlank();
+        assertThat(response.getRefreshToken()).isNotBlank();
+        assertThat(response.getMessage()).isEqualTo("Create Token");
     }
 }
