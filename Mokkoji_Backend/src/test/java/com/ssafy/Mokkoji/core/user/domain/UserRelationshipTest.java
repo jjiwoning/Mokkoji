1cad51150bb42c1b0b7443496866e4cd313f19a2
JJiWoNing <jjiwoning12@gmail.com>
2023-10-28 17:44:35 +0900

    test: UserRelationship 도메인 단위 테스트 추가

---
+++ Mokkoji_Backend/src/test/java/com/ssafy/Mokkoji/core/user/domain/UserRelationshipTest.java
@@ -0,0 +1,49 @@
+package com.ssafy.Mokkoji.core.user.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("UserRelationship 단위 테스트")
+class UserRelationshipTest {
+
+    @Test
+    @DisplayName("유저가 유저를 차단할 수 있다.")
+    void block() {
+        // given
+        User user = User.builder()
+                .build();
+        User targetUser = User.builder()
+                .build();
+
+        // when
+        UserRelationship userRelationship = UserRelationship.builder()
+                .user(user)
+                .targetUser(targetUser)
+                .relation(Relation.BLOCK)
+                .build();
+
+        // then
+        Assertions.assertThat(userRelationship).extracting(UserRelationship::getRelation).isEqualTo(Relation.BLOCK);
+    }
+
+    @Test
+    @DisplayName("유저가 유저를 팔로우할 수 있다.")
+    void follow() {
+        // given
+        User user = User.builder()
+                .build();
+        User targetUser = User.builder()
+                .build();
+
+        // when
+        UserRelationship userRelationship = UserRelationship.builder()
+                .user(user)
+                .targetUser(targetUser)
+                .relation(Relation.FOLLOW)
+                .build();
+
+        // then
+        Assertions.assertThat(userRelationship).extracting(UserRelationship::getRelation).isEqualTo(Relation.FOLLOW);
+    }
+}
