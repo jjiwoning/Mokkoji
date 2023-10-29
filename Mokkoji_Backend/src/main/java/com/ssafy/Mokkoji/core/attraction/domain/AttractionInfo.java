e9d71499f1bf12181a62a2818b7f4ec72ad32f80
JJiWoNing <jjiwoning12@gmail.com>
2023-10-29 22:33:44 +0900

    test: AttractionInfo 단위 테스트 추가

--- Mokkoji_Backend/src/main/java/com/ssafy/Mokkoji/core/attraction/domain/AttractionInfo.java
+++ Mokkoji_Backend/src/main/java/com/ssafy/Mokkoji/core/attraction/domain/AttractionInfo.java
@@ -1,6 +1,9 @@
 package com.ssafy.Mokkoji.core.attraction.domain;
 
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
@@ -8,6 +11,7 @@ import java.math.BigDecimal;
 @Table(name = "attraction_infos")
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class AttractionInfo {
 
 	@Id
@@ -34,5 +38,38 @@ public class AttractionInfo {
 	private BigDecimal latitude;
 	private BigDecimal longitude;
 	private String mlevel;
-	
+
+	@Builder
+	public AttractionInfo(
+			final int contentId,
+			final int contentTypeId,
+			final String title,
+			final String addr1,
+			final String addr2,
+			final String zipcode,
+			final String tel,
+			final String firstImage,
+			final String firstImage2,
+			final int readcount,
+			final Gugun gugun,
+			final BigDecimal latitude,
+			final BigDecimal longitude,
+			final String mlevel
+	) {
+		this.contentId = contentId;
+		this.contentTypeId = contentTypeId;
+		this.title = title;
+		this.addr1 = addr1;
+		this.addr2 = addr2;
+		this.zipcode = zipcode;
+		this.tel = tel;
+		this.firstImage = firstImage;
+		this.firstImage2 = firstImage2;
+		this.readcount = readcount;
+		this.sido = gugun.getSido();
+		this.gugun = gugun;
+		this.latitude = latitude;
+		this.longitude = longitude;
+		this.mlevel = mlevel;
+	}
 }
--- Mokkoji_Backend/src/main/java/com/ssafy/Mokkoji/core/attraction/domain/Sido.java
+++ Mokkoji_Backend/src/main/java/com/ssafy/Mokkoji/core/attraction/domain/Sido.java
@@ -1,5 +1,7 @@
 package com.ssafy.Mokkoji.core.attraction.domain;
 
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -8,13 +10,14 @@ import javax.persistence.*;
 @Table(name = "sidos")
 @Entity
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Sido {
 
 	@Id
 	private int sidoCode;
 	private String sidoName;
-	
+
+	@Builder
 	public Sido(
 			final int sidoCode,
 			final String sidoName
